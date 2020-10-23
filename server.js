162cbfddaf6bdf2cb1d6e8d5feaffb4660b42da9
jpcutshall <jpcutshall@gmail.com>
2020-10-23 11:33:37 -0400

    connected css and home.ejs

--- server.js
+++ server.js
@@ -1,3 +1,42 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const methodOverride = require('methodOverride')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const mongodbURI = process.env.MONGODBURI
+
+
+// middleWare
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
+
+
+// SESSIONS
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialiazed: false
+  })
+)
+
+// mongoose connection code
+mongoose.connect(mongodbURI, { useNewUrlParser: true})
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+// controllers
+
+
+app.get('/', (req,res) => {
+	res.render('home.ejs')   // pass collection of posts
+})
+
+app.listen(PORT, () => {
+	console.log('Listening to port : ', PORT)
+})
